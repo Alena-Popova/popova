2a66737eca406c30823be7774a585383d7c1bdcd
Alena-Popova <popova-alena1995@mail.ru>
2018-04-09 12:02:29 +0300

    Пояснения к задаче

--- chapter_002/src/main/java/ru/job4j/find_random/SelectionByWeight.java
+++ chapter_002/src/main/java/ru/job4j/find_random/SelectionByWeight.java
@@ -2,11 +2,21 @@ package popova.itmo_lesson.find_random;
 
 import java.util.Scanner;
 
+/**
+ * Дан массив чисел, и дан массив весов такой же длины. Задача: написать
+ * функцию, которая бы случайно выбирала число из первого массива с весом из
+ * второго.
+ *@author Helena
+ *@version 1.0
+ */
+
 public class SelectionByWeight {
     private String[] signMassiw;
     private String[] weightMassiw;
 
-
+    /**
+     * Введение двух массивов: символов и весов
+     */
     public void giveMassivAndWeight() {
         Scanner in = new Scanner(System.in);
         System.out.print("Введите массив символов: ");
@@ -20,6 +30,12 @@ public class SelectionByWeight {
         }
     }
 
+    /**
+     * найти массив. Генерим случайное число  (check )от 0 до суммы всех весов.
+     * если check <= rang, где ранг это постепенное увелечение границы веса для символа.
+     * то наше выбраное число найдено, если нет,то идем дальше
+     * @return
+     */
     public int findRandomValueByWeight() {
         int[] weightSumm = new int[this.weightMassiw.length];
         int finish = 0;
