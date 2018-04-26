2898364e5c8d6240581be5f3a15437ac741a9625
Alena-Popova <popova-alena1995@mail.ru>
2018-04-26 15:43:19 +0300

    Класс, который содержит о себе всю информацию о загруженной картинке: лейбл и массив байт - вектора пикселей

--- chapter_002/src/main/java/ru/job4j/characterRecognition/FileExceprion.java
+++
@@ -1,7 +0,0 @@
-package popova.characterRecognition;
-
-public class FileExceprion extends RuntimeException {
-    public FileExceprion(String fe) {
-        super(fe);
-    }
-}
---
+++ chapter_002/src/main/java/ru/job4j/characterRecognition/architecture/DataJPG.java
@@ -0,0 +1,134 @@
+package popova.characterRecognition.architecture;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+public class DataJPG {
+    private int lable;
+    private int[] linearVector;
+
+    /**
+     * В конструкторе записывается значение, которое принадлежит картинке
+     * и леаниризированная матрица (вектор)
+     * @param lable - число с картинки
+     * @param linearVector - вектор
+     */
+    public DataJPG(int lable, int[] linearVector) {
+        this.lable = lable;
+        this.linearVector = linearVector;
+    }
+
+    public int getLable() {
+        return this.lable;
+    }
+
+    public int[] getLinearVector() {
+        return this.linearVector;
+    }
+
+    /**
+     * Вычисляет длины вектора
+     * @return длена вектора
+     */
+    public double getLenght() {
+        double result = 0;
+        int[] coord = getLinearVector();
+        for (int coordinate = 0; coordinate < getLinearVector().length - 1 ; coordinate++) {
+            result += Math.pow(coord[coordinate] - coord[coordinate + 1], 2) + 1;
+        }
+        return Math.sqrt(result);
+    }
+
+    /**
+     * Вычисляет интеграл, описанный ветктором - площадь под ним
+     * @return вес веткора
+     */
+    public double getWeight() {
+        double result = 0;
+        int[] coord = getLinearVector();
+        for (int coordinate = 0; coordinate < getLinearVector().length; coordinate++) {
+
+            result += coord[coordinate];
+        }
+        return result;
+    }
+
+    /**
+     * вычисляте расстояние от вектора до вектора искомого обьекта  - DataJPG dataJPG
+     * @param dataJPG - обьект, который надо распознать
+     * @return расстояние от вектора до вектора ( всегдаполодительно )
+     */
+    public double getDifference(DataJPG dataJPG) {
+        double result = 0;
+        for (int coordinate = 0; coordinate < getLinearVector().length; coordinate++) {
+            result += Math.pow(this.getLinearVector()[coordinate] - dataJPG.getLinearVector()[coordinate], 2);
+        }
+        return Math.sqrt(result);
+    }
+
+    /**
+     * Получаем скалярное произведение векторов
+     * @param dataJPG
+     * @return
+     */
+    public double getScalar(DataJPG dataJPG) {
+        double result = 0;
+        for (int coordinate = 0; coordinate < getLinearVector().length; coordinate++) {
+            result += this.getLinearVector()[coordinate] * dataJPG.getLinearVector()[coordinate];
+        }
+
+        return result;
+    }
+
+    /**
+     * Получить модуль вектора
+     * @return
+     */
+    public double getModule() {
+        double result = 0;
+        for (int coordinate = 0; coordinate < getLinearVector().length; coordinate++) {
+            result += Math.pow(this.getLinearVector()[coordinate], 2);
+        }
+        return Math.sqrt(result);
+    }
+
+    public double getCos(DataJPG dataJPG) {
+        double result = 0;
+            result += this.getScalar(dataJPG)/(this.getModule() * dataJPG.getModule());
+        return result > 0 ? result : Math.abs(result) + 1;
+    }
+
+    /**
+     * переопределение equals для операций сравнения обьектов типа DataJPG
+     * @param o
+     * @return
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o && Arrays.equals(this.linearVector, ((DataJPG)o).getLinearVector())) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DataJPG dataJPG = (DataJPG) o;
+        return Objects.equals(lable, dataJPG.lable) &&
+                Arrays.equals(linearVector, dataJPG.linearVector);
+    }
+
+    @Override
+    public int hashCode() {
+
+        int result = Objects.hash(lable);
+        result = 31 * result + Arrays.hashCode(linearVector);
+        return result;
+    }
+
+    /**
+     * Выводит число, которым индетенфицированна картинка в библиотеке MNIST
+     * @return - лейбл
+     */
+    @Override
+    public String toString() {
+        return "DataJPG { "
+                + " lable="
+                + lable
+                + '}';
+    }
+}
