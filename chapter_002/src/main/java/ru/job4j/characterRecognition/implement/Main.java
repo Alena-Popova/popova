3e7648106d748070c39fb1cfd8bca8c282f39be7
Alena-Popova <popova-alena1995@mail.ru>
2018-04-26 15:54:30 +0300

    Запуск приложения. Подставляя нужный класс потока мы реализуем разные стратегии поиска.

---
+++ chapter_002/src/main/java/ru/job4j/characterRecognition/implement/Main.java
@@ -0,0 +1,25 @@
+package popova.characterRecognition.implement;
+
+import popova.characterRecognition.architecture.DataJPG;
+import popova.characterRecognition.architecture.Loader;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Main {
+
+    public static void main(String argv[]) throws IOException {
+
+        Loader loader = new Loader();
+        List<DataJPG> mnist = loader.getMNIST();
+        loader.getTestsImages();
+        List<DataJPG> tests = loader.getTest();
+        AtomicInteger atomicIterarion = new AtomicInteger();
+        AtomicInteger atomicError = new AtomicInteger();
+        for(int i = 0;i < 2;i++) {
+            new Thread(new RecognitionByCorner(mnist, tests, atomicIterarion, i, atomicError)).start();
+            // new Thread(new Recognition(mnist, tests, atomicIterarion, i, atomicError)).start();
+        }
+    }
+}
