201dc47f4eb092a21e60d2b76c4a365839d1edd9
Alena-Popova <popova-alena1995@mail.ru>
2018-04-26 15:53:04 +0300

    Класс реализающий поток, который ет картинку по методу минимального угла

---
+++ chapter_002/src/main/java/ru/job4j/characterRecognition/implement/RecognitionByCorner.java
@@ -0,0 +1,38 @@
+package popova.characterRecognition.implement;
+
+import popova.characterRecognition.architecture.DataJPG;
+import popova.characterRecognition.architecture.TheCornersStrategy;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class RecognitionByCorner extends ModelBehavior implements Runnable {
+
+    private AtomicInteger atomicIteration = new AtomicInteger();
+    private AtomicInteger atomicError= new AtomicInteger();
+
+    public RecognitionByCorner(List<DataJPG> mnist, List<DataJPG> test, AtomicInteger atomicIteration, int task, AtomicInteger atomicError) {
+        super(mnist, test, atomicIteration, task, atomicError);
+        this.atomicIteration = atomicIteration;
+        this.atomicError = atomicError;
+    }
+
+
+    @Override
+    public void run(){
+        TheCornersStrategy theCornersStrategy = new TheCornersStrategy(this.getMnist());
+        for (int i = this.getTask()*100; i < this.getTask()*100 + 100; i++) {
+            this.atomicIteration.incrementAndGet();
+            DataJPG newData = this.getTests().get(i);
+            if(newData.getLable() != theCornersStrategy.getMinNeighborbyCos(newData)) {
+                atomicError.incrementAndGet();
+            }
+
+            getPercentageCompletion( this.atomicIteration.get());
+            if(this.atomicIteration.get() == 199) {
+                getInfo(atomicError.get());
+            }
+        }
+    }
+
+}
